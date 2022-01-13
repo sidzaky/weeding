7e94525c84a6b995f5ac7069c31663252920e058
si.dzaky <si.dzaky@gmail.com>
2022-01-14 02:24:36 +0700

    fixing

--- application/controllers/Weeding.php
+++ application/controllers/Weeding.php
@@ -18,8 +18,21 @@ class Weeding extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+	function __construct() {
+
+		header('Access-Control-Allow-Origin: *');
+		header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+		header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+		$method = $_SERVER['REQUEST_METHOD'];
+		if($method == "OPTIONS") {
+			die();
+		}
+		parent::__construct();
+	}
+	
 	public function index()
 	{
+		
 		$this->load->library('user_agent');
 		$mobile=$this->agent->is_mobile();
 		if($mobile) {
